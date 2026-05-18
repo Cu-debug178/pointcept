e2604fef66d716664cead90776fbac2117549837
Cu-debug178 <1738202484@qq.com>
2026-05-18 21:05:38 +0800

    chore: remove nested Pointcept backup copy

--- pointcept/pointcept/models/kpconvx_hybrid/__init__.py
+++
@@ -1,9 +0,0 @@
-from .kpx_stage1 import KPConvXStage1
-from .kpx_stage2 import KPConvXStage2
-from .kpconvx_hybrid import KPConvXHybrid
-
-__all__ = [
-    "KPConvXStage1",
-    "KPConvXStage2",
-    "KPConvXHybrid",
-]
